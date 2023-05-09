55981632b4d97749e3b4bb15be3af7842d3ec0ab
Togu <timeofgetup@gmail.com>
2023-05-09 03:45:52 -0500

    Add tailwind

---
+++ Portfolio/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default Navbar
